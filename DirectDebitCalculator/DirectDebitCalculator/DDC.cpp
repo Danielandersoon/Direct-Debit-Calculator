fa8b3dee0b1603041dbaea0f78e3c5af1e7c79c3
Andy Andersoon <55076755+Danielandersoon@users.noreply.github.com>
2022-10-20 14:06:11 +0100

    DDC v0.2

--- DirectDebitCalculator/DirectDebitCalculator/DDC.cpp
+++ DirectDebitCalculator/DirectDebitCalculator/DDC.cpp
@@ -1,14 +1,25 @@
+/*
+*	This is a prototype open source project made by Daniel Anderson
+*	The aim is to make a calculator to find an aprox. DD for your energy
+*	bills based on prior usage and the prices of your current tariff.
+*
+*	Please do not use this for financial advice, I am in NO WAY qualified
+*	to speak on such matters. 
+*	Any harm caused by use of this program is soley the fault of the user.
+*/
+
 #include <iostream>
 #include <string>
 
+// Const declairations
 float mVol = 1.02264;
 float calorific = 39.4;
 float j_kW = 3.6;
 float metricConversion = 0.0283168;
 
+// variables used in runtime
 int NoMonths;
 int NoDays;
-
 bool m3;
 
 float ft_m(float ft)
@@ -34,6 +45,8 @@ float calculateMonthlyFuelCost(float fuelUsage, float standingCharge, float unit
 {
 	// uses aprox daily cost to find aprox yearly cost (in gbp)
 	float cost = calculateDailyFuelCost(fuelUsage, unitPrice);
+
+	// times daily cost of units by days in month
 	cost *= 30.f;
 	cost += standingCharge * 30.f;
 	return cost;
@@ -60,10 +73,12 @@ invalidInput:
 	}
 	else if (ism == 'N' || ism == 'n')
 	{
+		// First must convert from ft3 to m3
 		kWhGas = m_kWh(ft_m(gasIn));
 	}
 	else
 	{
+		// non valid input
 		cout << "Input not valid" << endl;
 		cout << "" << endl;
 		goto invalidInput;
@@ -75,20 +90,20 @@ invalidInput:
 
 	float gasStandingCharge;
 	float gasUnitRate;
-	cout << "Please input gas standing charge: ";
+	cout << "Please input gas standing charge in GBP: ";
 	cin >> gasStandingCharge;
 	cout << "" << endl;
-	cout << "Please input gas unit rate: ";
+	cout << "Please input gas unit rate in GBP: ";
 	cin >> gasUnitRate;
 	cout << "" << endl;
 	cout << "" << endl;
 
 	float elecStandingCharge;
 	float elecUnitRate;
-	cout << "Please input electric standing charge: ";
+	cout << "Please input electric standing charge in GBP: ";
 	cin >> elecStandingCharge;
 	cout << "" << endl;
-	cout << "Please input electric unit rate: ";
+	cout << "Please input electric unit rate in GBP: ";
 	cin >> elecUnitRate;
 	cout << "" << endl;
 	cout << "" << endl;
@@ -102,10 +117,34 @@ invalidInput:
 	cout << "" << endl;
 
 	float EDD = calculateMonthlyFuelCost(kWhGas, gasStandingCharge, gasUnitRate) + calculateMonthlyFuelCost(kWhElec, elecStandingCharge, elecUnitRate);
-	float EAC = EDD * 12.f;
+	float EAC = EDD * 12.f; // ~12 months 
 
-	cout << "EAC ~= " + to_string(EAC) << endl;
-	cout << "Recomended DD ~= " + to_string(EDD) << endl;
+	char accountForBall;
+	cout << "Would you like to take into account the acount ballance?: ";
+	cin >> accountForBall;
+
+	if (accountForBall == 'Y' || accountForBall == 'y')
+	{
+		float Ballance;
+		cout << "Please input ballance on account in GBP: "
+		cin >> Ballance;
+
+		Ballance /= 6; // assumes use over 6 months
+		cout << "EAC ~= " + to_string(EAC) << endl;
+		EDD -= Ballance;
+		cout << "Recomended DD ~= " + to_string(EDD) << endl;
+	}
+	else if (accountForBall == 'N' || accountForBall == 'n')
+	{
+		cout << "EAC ~= " + to_string(EAC) << endl;
+		cout << "Recomended DD ~= " + to_string(EDD) << endl;
+	}
+	else
+	{
+		cout << "Input not valid" << endl;
+		cout << "" << endl;
+		goto invalidInput2;
+	}
 
 	string a;
 	cin >> a;
